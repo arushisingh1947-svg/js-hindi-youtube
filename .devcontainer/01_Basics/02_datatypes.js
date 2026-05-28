475fe68d0044cfe1c488d54d0e166bef82b042d6
arushisingh1947-svg <arushisingh1947@gmail.com>
2026-05-28 18:32:27 +0000

    datatypes

---
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newwr version
+// alert(3+3) // we are using nodejs,not browser
+console.log(3+3)// code readability should be high
+console.log("Arushi")
+
+let name =  "Arushi"
+let age = 18
+let isLoggedIn = false
+// number=> 2  to power 53
+// bigint
+// string=
+// boolean = true/false
+// null = standalone value
+
+//undefined = value is not assigned till yet
+// symbol = unique
+//  object
+
+
+console.log(typeof null)
